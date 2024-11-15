6a5c25e9a0f91d25c3eadb5f9661d4b610dbe0b1
MSR-SakiburRahman <sakib2004011302@gmail.com>
2024-11-15 15:34:46 +0600

    Starting our Java Project

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is The new edited code!");
-        System.out.println("This is the new edit...");
-        System.out.println("We believe that we fixed it!");
-        System.out.println("We are still testing");
+        
     }
 }
